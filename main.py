73ea2fa3bedabb55a3db41c52fe8bf5088877ba7
nonaminho <eng.pedrotti@gmail.com>
2025-07-02 13:23:48 -0300

    Now we make reports

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from stats import *
 
-text = get_book_text("books/frankenstein.txt")
+book = "books/frankenstein.txt"
+text = get_book_text(book)
 num_words = count_words(text)
-print(f"{num_words} words found in the document")
+char_list = count_each(text)
 
-print(count_each(text))
+print(f"""============ BOOKBOT ============
+Analizing book found at {book}...
+----------- Word Count ----------
+Found {num_words} total words
+--------- Character Count -------""")
+
+for i in range(len(char_list)):
+    print(f"{char_list[i]['char']}: {char_list[i]['num']}")
+
+print("============= END ===============")
--- stats.py
+++ stats.py
@@ -7,15 +7,22 @@ def get_book_text(file_path):
 def count_words(text):
     return len(text.split())
 
+def sort_on(items):
+    return items['num']
 
 def count_each(text):
     count_dict={}
+    dict_list=[]
     text = text.lower()
     for char in text:
         if char in count_dict:   
             count_dict[char]+=1
         else:
             count_dict[char]=1
-            
-    
-    return count_dict
+
+    for entry in count_dict:
+        if entry.isalpha():
+            dict_list.append({'char':entry , 'num': count_dict[entry]})
+    dict_list.sort(reverse=True, key=sort_on)
+
+    return dict_list

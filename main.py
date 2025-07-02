9618d2b1d91d5f9255fb774e0dae2b4751682c9c
nonaminho <eng.pedrotti@gmail.com>
2025-07-01 22:37:59 -0300

    create the char count on stats

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from stats import get_book_text, count_words
+from stats import *
 
 text = get_book_text("books/frankenstein.txt")
 num_words = count_words(text)
 print(f"{num_words} words found in the document")
+
+print(count_each(text))
--- stats.py
+++ stats.py
@@ -6,3 +6,16 @@ def get_book_text(file_path):
 
 def count_words(text):
     return len(text.split())
+
+
+def count_each(text):
+    count_dict={}
+    text = text.lower()
+    for char in text:
+        if char in count_dict:   
+            count_dict[char]+=1
+        else:
+            count_dict[char]=1
+            
+    
+    return count_dict

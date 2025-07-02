a4671caf1d2914716cc8c5bb9fb03d9de1452792
nonaminho <eng.pedrotti@gmail.com>
2025-07-02 13:30:31 -0300

    add sys arguments

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from stats import *
+import sys
 
-book = "books/frankenstein.txt"
+if len(sys.argv)!=2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1) 
+
+book = sys.argv[1]
 text = get_book_text(book)
 num_words = count_words(text)
 char_list = count_each(text)
